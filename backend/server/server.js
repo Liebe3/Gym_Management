088452a8ec52ff8d3f90b275d4f2b8cb25eb0443
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-13 00:06:28 +0800

    add backend folder

---
+++ backend/server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("../config/db");
+const authRoutes = require("../routes/AuthRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const allowedOrigins = ["http://localhost:5173", "https://JPGym_management"];
+
+//middleware
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if(!origin){
+        return callback(null, true)
+      }
+      if(allowedOrigins.includes(origin)){
+        return callback(null, true)
+      }
+      else{
+        return callback(new Error("Not Allowed by CORS"))
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+//test Route
+app.get("/", (req, res) => {
+  res.send("Gym management API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server in running in Port${PORT}`));
