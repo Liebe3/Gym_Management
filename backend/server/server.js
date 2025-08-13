348002a8d6db268409e6e694faf74726c3a4a00b
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-13 23:31:28 +0800

    feature(server): add Member routes to main express app

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("../config/db");
 const authRoutes = require("../routes/AuthRoutes");
+const memberRoutes = require("../routes/MemberRoutes");
 
 dotenv.config();
 connectDB();
@@ -11,18 +12,16 @@ const app = express();
 const allowedOrigins = ["http://localhost:5173", "https://JPGym_management"];
 
 //middleware
-
 app.use(
   cors({
     origin: function (origin, callback) {
-      if(!origin){
-        return callback(null, true)
+      if (!origin) {
+        return callback(null, true);
       }
-      if(allowedOrigins.includes(origin)){
-        return callback(null, true)
-      }
-      else{
-        return callback(new Error("Not Allowed by CORS"))
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not Allowed by CORS"));
       }
     },
     credentials: true,
@@ -31,7 +30,7 @@ app.use(
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/members", memberRoutes);
 //test Route
 app.get("/", (req, res) => {
   res.send("Gym management API Running");
