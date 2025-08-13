f007655dab29d4049d9ab944f777fdf53fbd51a9
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-13 23:36:50 +0800

    feature(App): add MemberPage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import RegisterPage from "./pages/register/RegisterPage";
 import LoginPage from "./pages/login/LoginPage";
+import MembersPage from "./pages/member/MembersPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path={"/members"} element={<MembersPage />} />
         <Route path={"/register"} element={<RegisterPage />} />
         <Route path={"/login"} element={<LoginPage />} />c
       </Routes>
