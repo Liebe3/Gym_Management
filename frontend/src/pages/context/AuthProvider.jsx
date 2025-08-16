302578b6f222b7952559e5b008efad8db1851456
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-16 23:29:48 +0800

    feature(AuthProvider)
    create AuthProvider with login, register, and logout functionality

---
+++ frontend/src/pages/context/AuthProvider.jsx
@@ -0,0 +1,53 @@
+// hooks
+import { useEffect, useState } from "react";
+
+//context
+import AuthContext from "./AuthContext";
+
+//services
+import { AuthService } from "../../services/AuthService";
+const AuthProvider = ({ children }) => {
+  //state hook
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+    if (storedUser && storedToken) {
+      setUser(JSON.parse(storedUser));
+      setToken(storedToken);
+    }
+  }, []);
+
+  const register = async (formData) => {
+    return await AuthService.registerUser(formData);
+  };
+
+  const login = async (credentials) => {
+    const response = await AuthService.loginUser(credentials);
+
+    if (response.token) {
+      setUser(response.user);
+      localStorage.setItem("user", JSON.stringify(response.user));
+      setToken(response.token);
+      localStorage.setItem("token", response.token);
+    }
+    return response;
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
