fea8959139da91652154dc878f6d65a68c602e1b
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-17 21:18:57 +0800

    feature(main)
    import theme provider and wrapped it in app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 
 import AuthProvider from "./pages/context/AuthProvider.jsx";
+import ThemeProvider from "./pages/context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>
 );
