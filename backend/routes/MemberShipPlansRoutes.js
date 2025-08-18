42e1a3d9fb1e89be9d204f0a81e803b094594d31
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-19 05:24:00 +0800

    feature(MemberShipPlansRoutes)
    Add admin verification and updatePlan route for membership plans

--- backend/routes/MemberShipPlansRoutes.js
+++ backend/routes/MemberShipPlansRoutes.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
+const VerifyAdmin = require("../middleware/VerifyAdmin");
 
 const {
   createPlan,
   getAllPlans,
+  updatePlan,
 } = require("../controllers/MemberShipPlansController");
 
 router.use(VerifyToken);
+router.use(VerifyAdmin)
 
 router.get("/", getAllPlans);
 router.post("/", createPlan);
+router.put("/:id", updatePlan);
 
 module.exports = router;
