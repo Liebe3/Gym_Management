958f770876105e69e38c7c56a346f9773b5daec8
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-21 02:29:24 +0800

    feature(Api)
    create delete plan api function for membership plans

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -28,5 +28,6 @@ export const getAllPlans = () => API.get("/membership-plans");
 export const createPlan = (newPlan) => API.post("/membership-plans", newPlan);
 export const updatePlan = (id, updatedPlan) =>
   API.put(`/membership-plans/${id}`, updatedPlan);
+export const deletePlan = (id) => API.delete(`/membership-plans/${id}`)
 
 export default API;
