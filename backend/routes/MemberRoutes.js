f3ef03b363fb241bc412e7c47dd6728181eac0b4
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-23 05:49:31 +0800

    feature(MemberRoutes)
    add getAllMember route

--- backend/routes/MemberRoutes.js
+++ backend/routes/MemberRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
 const VerifyAdmin = require("../middleware/VerifyAdmin");
 
-const { createMember } = require("../controllers/MemberController");
+const { createMember, getAllMember } = require("../controllers/MemberController");
 
 router.use(VerifyToken);
 router.use(VerifyAdmin);
 
+router.get("/", getAllMember)
 router.post("/", createMember);
 
 
