5c8c86c9fe4c4516efa8c1867434dccc126045ae
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-22 08:09:55 +0800

    feature(server)
    create member base routes

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("../config/db");
 const authRoutes = require("../routes/AuthRoutes");
-const membershipPlansRoutes = require("../routes/MemberShipPlansRoutes"); 
+const membershipPlansRoutes = require("../routes/MemberShipPlansRoutes");
+const memberRoutes = require("../routes/MemberRoutes");
 
 dotenv.config();
 connectDB();
@@ -30,6 +31,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/membership-plans", membershipPlansRoutes);
+app.use("/api/member", memberRoutes);
 
 // Test route
 app.get("/", (req, res) => {
