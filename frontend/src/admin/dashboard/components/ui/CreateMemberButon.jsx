82d63789b9d77c75b323a7013e5754c966f92d42
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-25 23:07:35 +0800

    feature(CreateMemberButton)
    create button for member creation

---
+++ frontend/src/admin/dashboard/components/ui/CreateMemberButon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CreateMemberButon = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-emerald-600 text-white rounded cursor-pointer"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CreateMemberButon;
