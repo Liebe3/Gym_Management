58ec3c331352695f4d9f47e09611e8a9ab67eb4f
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-27 07:57:27 +0800

    feature(MemberRoutes)
    add route for checking user's active membership in Member router

--- backend/routes/MemberRoutes.js
+++ backend/routes/MemberRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
 const VerifyAdmin = require("../middleware/VerifyAdmin");
 
-const { createMember, getAllMember } = require("../controllers/MemberController");
+const { createMember, getAllMember, checkUserActiveMemberShip } = require("../controllers/MemberController");
 
 router.use(VerifyToken);
 router.use(VerifyAdmin);
 
 router.get("/", getAllMember)
 router.post("/", createMember);
+router.get("/check-active/:userId", checkUserActiveMemberShip)
 
 
-module.exports = router;
+module.exports = router; 
