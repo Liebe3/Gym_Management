c434b88dcd73d69c05ba394278a31ba3b8a38ad2
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-26 23:16:53 +0800

    rafactor(UserRoutes)
    add user route that can only access by admin

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
-const {
-  getMembers,
-  createMember,
-  updateMember,
-  deleteMember,
-  getMemberById,
-} = require("../controllers/MembersController");
+const VerifyAdmin = require("../middleware/VerifyAdmin");
 
-router.use(VerifyToken); // All routes require login
+const { getAllUser } = require("../controllers/UserController");
 
-router.get("/", getMembers);           // Admin only
-router.get("/:id", getMemberById);     // Admin or member themselves
-router.post("/", createMember);        // Admin only
-router.put("/:id", updateMember);      // Admin or member themselves
-router.delete("/:id", deleteMember);   // Admin only
+
+router.use(VerifyToken);
+router.use(VerifyAdmin);
+
+router.get("/", getAllUser);
 
 module.exports = router;
