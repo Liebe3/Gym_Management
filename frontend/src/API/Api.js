516df717d859a712cfb52f9c18e6411e468b3395
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-29 07:02:44 +0800

    feature(Api)
    add deletedMember helper for member deletion

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -39,6 +39,7 @@ export const getAllMember = () => API.get("/member");
 export const createMember = (newMember) => API.post("/member", newMember);
 export const updateMember = (id, updateMember) => API.put(`/member/${id}`, updateMember); // Add this
 export const checkUserActiveMemberShip = (userId) => API.get(`/member/check-active/${userId}`)
+export const deletedMember = (id) => API.delete(`/member/${id}`)
 
 
 export default API;
