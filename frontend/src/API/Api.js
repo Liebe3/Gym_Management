321ca642713be96e3eb10d56046635299621efba
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-08-28 09:31:23 +0800

    feature(Api)
    add updateMember function to handle PUT requests for members

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -37,6 +37,7 @@ export const getAllUser = () => API.get("/user")
 //member API
 export const getAllMember = () => API.get("/member");
 export const createMember = (newMember) => API.post("/member", newMember);
+export const updateMember = (id, updateMember) => API.put(`/member/${id}`, updateMember); // Add this
 export const checkUserActiveMemberShip = (userId) => API.get(`/member/check-active/${userId}`)
 
 
