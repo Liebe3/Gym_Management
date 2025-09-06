18387271cb83b03e24f2d480aabdb3741f58757e
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-06 19:12:26 +0800

    feature(UserController)
    add createUser function to handle user creation

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -40,3 +40,31 @@ exports.getAllUser = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch users", error });
   }
 };
+
+exports.createUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exist" });
+    }
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      role,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
