86784ac74cd670cb7ac80accea1b465808288bd1
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-09 13:23:20 +0800

    feature(UserController)
    add a delete function for user delete

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -51,7 +51,7 @@ exports.createUser = async (req, res) => {
       return res.status(400).json({ message: "Email already exist" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
       firstName,
@@ -67,6 +67,23 @@ exports.createUser = async (req, res) => {
       success: true,
       message: "User created successfully",
     });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "User delete successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
