32066ed0bfcb745dbbb2df4437e4a70ce9285779
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-10 16:37:16 +0800

    feature: add route for delete
    create route definition for delete method

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
 const VerifyAdmin = require("../middleware/VerifyAdmin");
 
-const { getAllUser, createUser, deleteUser } = require("../controllers/UserController");
+const {
+  getAllUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require("../controllers/UserController");
 
 router.use(VerifyToken);
 router.use(VerifyAdmin);
 
 router.get("/", getAllUser);
-router.post("/", createUser)
-router.delete("/:id", deleteUser)
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
