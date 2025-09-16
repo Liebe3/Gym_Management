333527d8ebfe6cc8bd6e1d219d9f951130709c8c
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-16 19:50:12 +0800

    feature: routes
    add routes get method

--- backend/routes/TrainerRoutes.js
+++ backend/routes/TrainerRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
 const VerifyAdmin = require("../middleware/VerifyAdmin");
 
-const { createTrainer } = require("../controllers/TrainerController");
+const { createTrainer, getAllTrainer} = require("../controllers/TrainerController");
 
 router.use(VerifyToken);
 router.use(VerifyAdmin);
 
+router.get("/", getAllTrainer)
 router.post("/", createTrainer);
 
 module.exports = router;
