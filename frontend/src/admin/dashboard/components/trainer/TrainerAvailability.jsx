d314066f3905ff89c7c91f5d1bc6878e6ae55295
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-20 18:45:32 +0800

    style: availabitiy
    change the min width value and cursor to pointer

--- frontend/src/admin/dashboard/components/trainer/TrainerAvailability.jsx
+++ frontend/src/admin/dashboard/components/trainer/TrainerAvailability.jsx
@@ -8,19 +8,19 @@ const TrainerAvailability = ({ value, onChange, disabled }) => {
       <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
         <div className="flex items-center">
           <FiCalendar className="w-4 h-4 mr-1 text-emerald-600" />
-          Availability
+          Availability for new clients
         </div>
       </label>
 
       {disabled ? (
-        <div className="w-fit min-w-[160px] rounded-lg p-2 border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 text-gray-500 dark:text-gray-400 text-sm shadow-sm cursor-not-allowed">
+        <div className="w-fit min-w-[190px] text-center rounded-lg p-2 border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 text-gray-500 dark:text-gray-400 text-sm shadow-sm cursor-not-allowed">
           {displayValue.charAt(0).toUpperCase() + displayValue.slice(1)}
         </div>
       ) : (
         <select
           value={displayValue}
           onChange={(event) => onChange(event.target.value === "available")}
-          className="w-fit min-w-[160px] rounded-lg p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white text-sm shadow-sm focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+          className="w-fit min-w-[190px] rounded-lg p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white text-sm shadow-sm focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 cursor-pointer"
         >
           <option value="">Select</option>
           <option value="available">Available</option>
