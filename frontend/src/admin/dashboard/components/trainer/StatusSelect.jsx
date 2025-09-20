6ddce2272a010fa997a7901c71cd3928bf4b0449
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-20 18:47:54 +0800

    refactor: status
    update e to event for much better readablity

--- frontend/src/admin/dashboard/components/trainer/StatusSelect.jsx
+++ frontend/src/admin/dashboard/components/trainer/StatusSelect.jsx
@@ -6,8 +6,8 @@ const StatusSelect = ({ value, onChange }) => {
       </label>
       <select
         value={value}
-        onChange={(e) => onChange(e.target.value)}
-        className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg dark:bg-gray-800 dark:text-white outline-emerald-500"
+        onChange={(event) => onChange(event.target.value)}
+        className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg dark:bg-gray-800 dark:text-white outline-emerald-500 cursor-pointer"
       >
         <option value="active">Active</option>
         <option value="inactive">Inactive</option>
