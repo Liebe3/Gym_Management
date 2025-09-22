c0649c16d3f4a6c25acb8bc8d70f8c36083e7abf
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-22 22:26:42 +0800

    feature: gender
    add disabled in mode view

--- frontend/src/admin/dashboard/components/trainer/GenderSelect.jsx
+++ frontend/src/admin/dashboard/components/trainer/GenderSelect.jsx
@@ -1,4 +1,4 @@
-const GenderSelect = ({ value, onChange }) => {
+const GenderSelect = ({ value, onChange, disabled }) => {
   return (
     <div>
       <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
@@ -8,7 +8,13 @@ const GenderSelect = ({ value, onChange }) => {
         value={value}
         onChange={(event) => onChange(event.target.value)}
         required
-        className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg dark:bg-gray-800 dark:text-white outline-emerald-500 cursor-pointer"
+        disabled={disabled}
+        className={`w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg outline-emerald-500
+    ${
+      disabled
+        ? "bg-gray-100 dark:bg-gray-900 text-gray-400 dark:text-gray-400 cursor-not-allowed"
+        : "bg-white dark:bg-gray-800 text-gray-900 dark:text-white cursor-pointer"
+    }`}
       >
         <option value="">Select Gender</option>
         <option value="male">Male</option>
