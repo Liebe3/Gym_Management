dea185282b896e6ce47cdd21f72965e089163bea
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-22 20:24:18 +0800

    feature: Api
    add api getTrainerId, update and delete function

--- frontend/src/API/trainerApi.js
+++ frontend/src/API/trainerApi.js
@@ -2,4 +2,8 @@ import API from "./Api";
 
 //trainer API
 export const getAllTrainer = () => API.get("/trainer");
+export const getTrainerId = (id) => API.get(`/trainer/${id}`);
 export const createTrainer = (newTrainer) => API.post("/trainer", newTrainer);
+export const updateTrainer = (id, updateTrainer) =>
+  API.put(`/trainer${id}`, updateTrainer);
+export const deleteTrainer = (id) => API.delete(`/trainer/${id}`);
