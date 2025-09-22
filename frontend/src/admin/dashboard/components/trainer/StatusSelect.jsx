d56206016bb7e58dd6a90c9b1429c08fdde1ce75
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-22 22:28:03 +0800

    feature: status
    add disabled in mode view

--- frontend/src/admin/dashboard/components/trainer/StatusSelect.jsx
+++ frontend/src/admin/dashboard/components/trainer/StatusSelect.jsx
@@ -1,4 +1,4 @@
-const StatusSelect = ({ value, onChange }) => {
+const StatusSelect = ({ value, onChange, disabled }) => {
   return (
     <div>
       <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
@@ -7,7 +7,13 @@ const StatusSelect = ({ value, onChange }) => {
       <select
         value={value}
         onChange={(event) => onChange(event.target.value)}
-        className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg dark:bg-gray-800 dark:text-white outline-emerald-500 cursor-pointer"
+        disabled={disabled}
+        className={`w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg outline-emerald-500
+    ${
+      disabled
+        ? "bg-gray-100 dark:bg-gray-900 text-gray-400 dark:text-gray-400 cursor-not-allowed"
+        : "bg-white dark:bg-gray-800 text-gray-900 dark:text-white cursor-pointer"
+    }`}
       >
         <option value="active">Active</option>
         <option value="inactive">Inactive</option>
