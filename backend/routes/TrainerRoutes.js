4e8369bea63ba5110cdc1061420a049c4e966386
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-22 20:22:55 +0800

    feature: routes
    add get by id, update and delete routes for trainer

--- backend/routes/TrainerRoutes.js
+++ backend/routes/TrainerRoutes.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const VerifyToken = require("../middleware/VerifyToken");
 const VerifyAdmin = require("../middleware/VerifyAdmin");
 
-const { createTrainer, getAllTrainer} = require("../controllers/TrainerController");
+const {
+  createTrainer,
+  getAllTrainer,
+  updateTrainer,
+  getTrainerById,
+  deleteTrainer,
+} = require("../controllers/TrainerController");
 
 router.use(VerifyToken);
 router.use(VerifyAdmin);
 
-router.get("/", getAllTrainer)
+router.get("/", getAllTrainer);
+router.get("/:id", getTrainerById);
 router.post("/", createTrainer);
+router.put("/:id", updateTrainer);
+router.delete("/:id", deleteTrainer)
 
 module.exports = router;
