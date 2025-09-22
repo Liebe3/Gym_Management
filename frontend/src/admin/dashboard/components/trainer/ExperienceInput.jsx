dfc2a49775726a69797cc9ae4218c6e321630d38
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-22 22:26:24 +0800

    feature: experince
    add disabled in mode view

--- frontend/src/admin/dashboard/components/trainer/ExperienceInput.jsx
+++ frontend/src/admin/dashboard/components/trainer/ExperienceInput.jsx
@@ -1,4 +1,4 @@
-const ExperienceInput = ({ value, onChange }) => {
+const ExperienceInput = ({ value, onChange, disabled }) => {
   return (
     <div>
       <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
@@ -10,7 +10,13 @@ const ExperienceInput = ({ value, onChange }) => {
         required
         value={value}
         onChange={(event) => onChange(parseInt(event.target.value) || 0)}
-        className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg dark:bg-gray-800 dark:text-white outline-emerald-500 cursor-pointer"
+        disabled={disabled}
+        className={`w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg outline-emerald-500
+    ${
+      disabled
+        ? "bg-gray-100 dark:bg-gray-900 text-gray-400 dark:text-gray-400 cursor-not-allowed"
+        : "bg-white dark:bg-gray-800 text-gray-900 dark:text-white cursor-pointer"
+    }`}
       />
     </div>
   );
