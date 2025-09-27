d0610892ddf7dbbb3fbed4468b451caa0ac895a2
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-27 21:06:57 +0800

    feature: dashboard
    add dashboard

---
+++ frontend/src/admin/dashboard/dashboard.jsx
@@ -0,0 +1,12 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Dashboard Overview</h1>
+      <p className="text-gray-600 dark:text-gray-300">
+        Welcome to the Gym Management Admin Dashboard.
+      </p>
+    </div>
+  );
+};
+
+export default Dashboard;
