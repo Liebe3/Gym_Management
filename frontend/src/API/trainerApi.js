791089ec12abfb96be118ebb2bafa8b196c9786c
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-10-02 23:50:33 +0800

    feature: APi
    add new api service layer for trainer stats

--- frontend/src/API/trainerApi.js
+++ frontend/src/API/trainerApi.js
@@ -3,6 +3,7 @@ import API from "./Api";
 //trainer API
 export const getAllTrainer = () => API.get("/trainer");
 export const getTrainerId = (id) => API.get(`/trainer/${id}`);
+export const getTrainerStatus = (id) => API.get(`/trainer${id}/stats`)
 export const createTrainer = (newTrainer) => API.post("/trainer", newTrainer);
 export const updateTrainer = (id, updateTrainer) =>
   API.put(`/trainer${id}`, updateTrainer);
