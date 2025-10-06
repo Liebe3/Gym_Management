17a422f1ac6fa069180a734f48d1f890fabecea7
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-10-06 10:56:21 +0800

    Update trainerApi.js

--- frontend/src/API/trainerApi.js
+++ frontend/src/API/trainerApi.js
@@ -1,10 +1,10 @@
-import API from "./Api";
+// import API from "./Api";
 
-//trainer API
-export const getAllTrainer = () => API.get("/trainer");
-export const getTrainerId = (id) => API.get(`/trainer/${id}`);
-export const getTrainerStatus = (id) => API.get(`/trainer${id}/stats`)
-export const createTrainer = (newTrainer) => API.post("/trainer", newTrainer);
-export const updateTrainer = (id, updateTrainer) =>
-  API.put(`/trainer${id}`, updateTrainer);
-export const deleteTrainer = (id) => API.delete(`/trainer/${id}`);
+// //trainer API
+// export const getAllTrainer = () => API.get("/trainer");
+// export const getTrainerId = (id) => API.get(`/trainer/${id}`);
+// export const getTrainerStats = (id) => API.get(`/trainer/${id}/stats`)
+// export const createTrainer = (newTrainer) => API.post("/trainer", newTrainer);
+// export const updateTrainer = (id, updateTrainer) =>
+//   API.put(`/trainer${id}`, updateTrainer);
+// export const deleteTrainer = (id) => API.delete(`/trainer/${id}`);
