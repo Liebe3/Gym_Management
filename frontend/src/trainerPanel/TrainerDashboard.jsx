8cb29af682fffe667754374d31c93eb8517bc86d
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-10-29 21:43:22 +0800

    feature: TrainerDashboard
    add new component my trainer dashboard for trainer panel

---
+++ frontend/src/trainerPanel/TrainerDashboard.jsx
@@ -0,0 +1,14 @@
+import TrainerLayout from "./components/TrainerLayout";
+
+const TrainerDashboard = () => {
+  return (
+    <TrainerLayout>
+      <div>
+        <h1 className="text-2xl font-bold">Trainer Dashboard</h1>
+        <p>Welcome to your dashboard!</p>
+      </div>
+    </TrainerLayout>
+  );
+};
+
+export default TrainerDashboard;
