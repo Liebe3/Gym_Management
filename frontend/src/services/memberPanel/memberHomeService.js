7f598e3238e958b66b6f45278bac4ab3a0c69d3d
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-12-09 15:21:02 +0800

    feature: memberHomeService
    add service method for fetching assigned trainers

--- frontend/src/services/memberPanel/memberHomeService.js
+++ frontend/src/services/memberPanel/memberHomeService.js
@@ -1,12 +1,11 @@
 import API from "../../API/Api";
 
 const memberHomeService = {
-
   // Applied DRY
   getMemberHomeData: async () => {
     try {
       const response = await API.get("/member-panel/member/home-data");
-      return response.data.data; 
+      return response.data.data;
     } catch (error) {
       console.error(
         "Service error fetching member home data:",
@@ -90,6 +89,16 @@ const memberHomeService = {
         "Service error fetching top trainers:",
         error.response?.data || error.message
       );
+      throw error;
+    }
+  },
+
+  getAssignedTrainers: async () => {
+    try {
+      const response = await API.get("/member-panel/assigned-trainers");
+      return response.data.data;
+    } catch (error) {
+      console.error("Service error fetching assigned trainers:", error);
       throw error;
     }
   },
