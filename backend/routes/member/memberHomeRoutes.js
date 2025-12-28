86798156b5161cdd50bbe626015bb0e9e89fb4c0
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-12-28 12:55:06 +0800

    feature: memberSessionRoutes
    created a route for memberSessionRoutes

--- backend/routes/member/memberHomeRoutes.js
+++ backend/routes/member/memberHomeRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const VerifyToken = require("../../middleware/VerifyToken");
 const VerifyMember = require("../../middleware/VerifyMember");
 
-
-const {  getActiveMemberHomeData,
+const {
+  getActiveMemberHomeData,
+  getAvailableTrainers,
 } = require("../../controllers/member/MemberHomeController");
 
 router.use(VerifyToken);
 router.use(VerifyMember);
 
 router.get("/member/home-data", getActiveMemberHomeData);
+router.get("/member/available-trainers", getAvailableTrainers);
 
 module.exports = router;
