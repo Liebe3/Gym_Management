f21c45f87088fc44ffcfc064bcf0aae1783216e2
dyaaah <95646179+Liebe3@users.noreply.github.com>
2026-01-02 21:34:16 +0800

    feature: routes
    add routes for cancellation of session

--- backend/routes/trainer/TrainerSessionRoutes.js
+++ backend/routes/trainer/TrainerSessionRoutes.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const VerifyToken = require('../../middleware/VerifyToken');
-const VerifyTrainer = require('../../middleware/VerifyTrainer');
-
+const VerifyToken = require("../../middleware/VerifyToken");
+const VerifyTrainer = require("../../middleware/VerifyTrainer");
 
 router.use(VerifyToken);
 router.use(VerifyTrainer);
 
-const {getMySessions, getMySessionById, createMySession, updateMySession} = require("../../controllers/trainer/TrainerSessionController");
+const {
+  getMySessions,
+  getMySessionById,
+  createMySession,
+  updateMySession,
+  cancelMySession,
+} = require("../../controllers/trainer/TrainerSessionController");
 
-router.get('/', getMySessions);
-router.get('/:sessionId', getMySessionById);
-router.post('/', createMySession); 
-router.put('/:sessionId', updateMySession);
+router.get("/", getMySessions);
+router.get("/:sessionId", getMySessionById);
+router.post("/", createMySession);
+router.put("/:sessionId", updateMySession);
+router.put("/:sessionId/cancel", cancelMySession);
 
 module.exports = router;
